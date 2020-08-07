b1c00ef0d02bce0d8219a95929056428cf459e5f
pangesturoni <pangesturoni@gmail.com>
2020-08-07 10:09:04 +0700

    database migrations

---
+++ database/migrations/2020_08_06_235904_create_jawaban_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->LongText('isi');
+            $table->date('tanggal_dibuat')->nullable();
+            $table->date('tanggal_diperbaharui')->nullable();
+
+            $table->unsignedBigInteger('profil_id');
+            $table->foreign('profil_id')->references('id')->on('profil');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jawaban');
+    }
+}
